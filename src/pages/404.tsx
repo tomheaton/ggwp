2298f269b247626f2ff5f3093c9c6441b0d46e3c
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-09-22 10:43:13 +0100

    work :gear:

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,10 @@
 import styles from "../styles/Error.module.css";
 import Link from "next/link";
+import {NextPage} from "next";
 
-const Error = () => {
+const Error: NextPage = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <h1 className={styles.title}>malformed link</h1>
             <Link href={"/"}>Return Home</Link>
         </div>
--- src/pages/ggwp.tsx
+++ src/pages/ggwp.tsx
@@ -1,10 +1,42 @@
 import styles from "../styles/GGWP.module.css";
 import Link from "next/link";
+import {useRouter} from "next/router";
+import {NextPage} from "next";
 
-const GGWP = () => {
+const GGWP: NextPage = () => {
+
+    const router = useRouter();
+    const {to, from} = router.query;
+
+    const copy = () => {
+        console.log("link copied to clipboard");
+    }
+
+    if (!router.query) {
+        return (
+            <div className={styles.container}>
+                <p>loading</p>
+            </div>
+        );
+    } else if (!to || !from) {
+        return (
+            <div className={styles.container}>
+                <p>invalid url params</p>
+                <Link href={"/"}>Return Home</Link>
+            </div>
+        );
+    }
+
     return (
-        <div>
+        <div className={styles.container}>
             <h1 className={styles.title}>ggwp</h1>
+
+            <div>
+                <p>To: {to}</p>
+                <p>From: {from}</p>
+            </div>
+
+            <button onClick={copy}>Copy</button>
         </div>
     );
 }
