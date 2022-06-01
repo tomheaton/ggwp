07792db332b5074f6a372d89bec5dc18cd333adf
Tom Heaton <thomas.m.heaton@gmail.com>
2022-06-01 23:18:41 +0100

    improvements

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,17 @@
 import styles from "../styles/Error.module.css";
 import {NextPage} from "next";
 import Link from "next/link";
+import Head from "next/head";
 
 const Error: NextPage = () => {
 
     return (
         <div className={styles.container}>
+            <Head>
+                <title>Error | ggwp</title>
+                <meta name="description" content="Create a simple ggwp message to share with your friends!"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
             <h1 className={styles.title}>
                 malformed link
             </h1>
--- src/pages/ggwp.tsx
+++ src/pages/ggwp.tsx
@@ -2,6 +2,7 @@ import styles from "../styles/GGWP.module.css";
 import {useRouter} from "next/router";
 import {NextPage} from "next";
 import Link from "next/link";
+import Head from "next/head";
 
 const GGWP: NextPage = () => {
 
@@ -17,6 +18,12 @@ const GGWP: NextPage = () => {
     if (!router.query) {
         return (
             <div className={styles.container}>
+                <Head>
+                    <title>loading... | ggwp</title>
+                    <meta name="description" content="Create a simple ggwp message to share with your friends!"/>
+                    <link rel="icon" href="/favicon.ico"/>
+                </Head>
+
                 <p>loading</p>
             </div>
         );
@@ -25,6 +32,11 @@ const GGWP: NextPage = () => {
     if (!to || !from) {
         return (
             <div className={styles.container}>
+                <Head>
+                    <title>invalid url params | ggwp</title>
+                    <meta name="description" content="Create a simple ggwp message to share with your friends!"/>
+                    <link rel="icon" href="/favicon.ico"/>
+                </Head>
                 <p>
                     invalid url params
                 </p>
@@ -39,15 +51,20 @@ const GGWP: NextPage = () => {
 
     return (
         <div className={styles.container}>
+            <Head>
+                <title>To {to}, From {from} | ggwp</title>
+                <meta name="description" content="Create a simple ggwp message to share with your friends!"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
             <h1 className={styles.title}>
                 ggwp
             </h1>
-
+            <br/>
             <div>
                 <p>To: {to}</p>
                 <p>From: {from}</p>
             </div>
-
+            <br/>
             <button onClick={copy}>
                 Copy
             </button>
